57deeec089e243a9077dc48acc7092e069031174
Michel Hidalgo <michel@ekumenlabs.com>
2021-04-16 17:07:40 -0300

    Add missing package_dir in setup.py
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name=package_name,
     version='1.0.2',
     packages=[package_name],
+    package_dir={'': 'src'},
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
